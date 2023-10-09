534b951aa06cbf98d70b69a9b7ddb4cc4d6b32b5
BardanIrina <irinabardan2003@yahoo.com>
2023-10-09 14:53:48 +0300

    change two

--- src/src2023/seminar/group911/seminar_2.py
+++ src/src2023/seminar/group911/seminar_2.py
@@ -1,2 +1,34 @@
-print('hello')
-print('is this working?')
+#print('hello')
+#print('is this working?')
+""""
+
+Manage a list of circles
+    1. Add a circle from console
+    2. Add a random circle
+    3. Display all circles
+    4. Sort circle by radius
+    5. Exit the program
+
+"""
+
+print('Hello world')
+
+#a circlus is represented by center (x,y) and radius (r) -- all integers, r > 0
+
+c1 = {"x": 1, "y": 2, "r": 3}
+c2 = {"x": 3, "y": 4, "r": 5}
+print([c1,c2])
+#circle center (1,2), radius 3 => {"x":1, "y":2, "r":3}
+
+#Decouple the circle's representation from the prgogram
+#the program should not know that the circle is a dict
+#Circle functions
+
+
+#Program funtionalities
+
+d = {1: "Alice", 2: "Bob", 3: [1,2,3]}
+#d[3] = "Xavier"
+#print(d, type (d))
+#print(d.keys(), d.values())
+
